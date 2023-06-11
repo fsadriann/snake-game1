e8c1d9d4f660ce9906bd0e7dc2e1e06deb86367d
unknown <adrianforero@colegioguanenta.edu.co>
2023-06-10 22:05:27 -0500

    added keyboard functions

--- snake-game1.py
+++ snake-game1.py
@@ -1,10 +1,60 @@
 import turtle
 import time
-import random
 
+posponer = 0.1
+
+
+# configuramos la ventana
 window = turtle.Screen()
-window.title = "a"
+window.title("Snake 1")
+window.bgcolor("gray5")
+window.setup(width=500, height=500)
+window.tracer(0)
 
+ # cabeza serpiente
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green yellow")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
+#funciones
+def go_up():
+    head.direction = "up"
+def go_down():
+    head.direction = "down"
+def go_left():
+    head.direction = "left"
+def go_right():
+    head.direction = "right"
 
-window.mainloop()
+def mov():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+#teclado
+window.listen()
+window.onkeypress(go_up, "Up")
+window.onkeypress(go_down, "Down")
+window.onkeypress(go_left, "Left")
+window.onkeypress(go_right, "Right")
+    
+
+#creamos el bucle principal
+while True:
+    window.update()
+
+    mov()
+    time.sleep(posponer)
