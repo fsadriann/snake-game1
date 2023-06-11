9be56301665f090be75d15c9115f9a33c114232d
unknown <adrianforero@colegioguanenta.edu.co>
2023-06-10 21:32:45 -0500

    testing turtle library

---
+++ snake-game1.py
@@ -0,0 +1,10 @@
+import turtle
+import time
+import random
+
+window = turtle.Screen()
+window.title = "a"
+
+
+
+window.mainloop()
---
+++ test-turtle.py
@@ -0,0 +1,11 @@
+import turtle
+
+flecha = turtle.Turtle()
+
+"""flecha.forward(100)
+flecha.left(90)
+flecha.forward(100)"""
+
+for i in range(0,4):
+    flecha.forward(100)
+    flecha.left(90)
